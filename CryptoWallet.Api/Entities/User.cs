2d3629d32d5191c7c3fb54e0453449c93f4099d3
Egor <petr616@yandex.ru>
2025-08-08 10:42:30 +0300

    fix

--- CryptoWallet.Api/Entities/User.cs
+++ CryptoWallet.Api/Entities/User.cs
@@ -4,13 +4,12 @@ namespace CryptoWallet.Api.Entities;
 
 public class User
 {
-    [Key]
+
     public Guid UserId { get; set; }
 
-    [Required]
-    [EmailAddress]
+
     public string Email { get; set; } = string.Empty;
 
-    [Range(0, double.MaxValue)]
+
     public double Balance { get; set; } = 0;
 }
